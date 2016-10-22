b8983e827bbff5e305f478fb57b3cd90cd27f334
Dustin Reed <dmreedy1@gmail.com>
2016-10-22 09:15:43 -0500

    switching computers again

---
+++ js/Robot.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const Robot = (function functionName() {
+
+  let newRobot = {};
+
+  newRobot.robot = ()=>{
+    this.name = "";
+  };
+
+  return newRobot;
+
+})();
---
+++ js/models.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var Robot = (function (robot){
+
+robot.models = {};
+
+
+
+
+
+
+
+return robot;
+})(Robot || {});
